4847307ad56eea0127ed6bbd20ebae7a67c4b4da
lemuelpaz <lemuelpazcontato@hotmail.com>
2022-05-18 22:38:16 -0300

    Sistema de Soma

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sistema_de_Soma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, Somando;
+
+            Console.Write("Informe o Primeiro Número: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o Segundo Número: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Somando = numero1 + numero2;
+
+            Console.Write("Resultado = " + Somando);
+            Console.ReadKey();
+
+        }
+    }
+}
